d1c5d324d2d1bc1f351edf5e4a135dad7caa5704
msakuta <masahiro.sakuta@gmail.com>
2022-02-06 18:18:00 +0900

    Add circle example

---
+++ examples/circle.rs
@@ -0,0 +1,31 @@
+use image::{ImageBuffer, Luma};
+use rust_edt::edt;
+
+fn main() {
+    const SIZE: usize = 128;
+    const HALFSIZE: usize = SIZE / 2;
+
+    let mut map = vec![false; SIZE * SIZE];
+
+    for i in 0..SIZE {
+        for j in 0..SIZE {
+            let dx = j - HALFSIZE;
+            let dy = i - HALFSIZE;
+            map[j + i * SIZE] = dx * dx + dy * dy < HALFSIZE * HALFSIZE;
+        }
+    }
+
+    let edt_f64 = edt(&map, (SIZE, SIZE));
+
+    let max_value = edt_f64.iter().map(|p| *p).reduce(f64::max).unwrap();
+    let edt_img = edt_f64
+        .iter()
+        .map(|p| (*p / max_value * 255.) as u8)
+        .collect();
+
+    let edt_img: ImageBuffer<Luma<u8>, Vec<u8>> =
+        ImageBuffer::from_vec(SIZE as u32, SIZE as u32, edt_img).unwrap();
+
+    // Write the contents of this image to the Writer in PNG format.
+    edt_img.save("edt.png").unwrap();
+}
