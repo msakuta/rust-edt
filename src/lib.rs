bcf80780620af5491be8350576d9b100f9cc53a8
msakuta <masahiro.sakuta@gmail.com>
2022-02-06 19:50:32 +0900

    Add documentation comments to the source

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,72 @@
+//! # edt
+//!
+//! An implementation of 2D EDT ([Euclidian distance transform](https://en.wikipedia.org/wiki/Distance_transform)) with Saito's algorithm in pure Rust
+//!
+//! EDT is the basis of many algorithms, but it is hard to find in a general purpose image processing library.
+//! The algorithm is not trivial to implement efficiently.
+//! This crate provides an implementation of EDT in fairly efficient algorithm presented in the literature.
+//!
+//! The algorithm used in this crate (Saito's algorithm) is O(n^3), where n is the number of pixels along one direction.
+//! Naive computation of EDT would be O(n^4), so it is certainly better than that, but there is also fast-marching based
+//! algorithm that is O(n^2).
+//!
+//! ## Usage
+//!
+//! Add dependency
+//!
+//! ```toml
+//! [dependencies]
+//! edt = "0.1.0"
+//! ```
+//!
+//! Prepare a binary image as a flattened vec.
+//! This library assumes that the input is a flat vec for 2d image.
+//!
+//! ```rust
+//! let vec: Vec<u8> = vec![/*...*/];
+//! ```
+//!
+//! Call edt with given shape
+//!
+//! ```rust
+//! # let vec: Vec<u8> = vec![/*...*/];
+//! use edt::edt;
+//!
+//! let edt_image = edt(&vec, (32, 32));
+//! ```
+//!
+//! Save to a file if you want.
+//! The code below normalizes the value with maximum value to 8 bytes grayscale image.
+//!
+//! ```rust
+//! # use edt::edt;
+//! # let vec: Vec<u8> = vec![/*...*/];
+//! # let edt_image = edt(&vec, (32, 32));
+//! use image::{ImageBuffer, Luma};
+//!
+//! let max_value = edt_image.iter().map(|p| *p).reduce(f64::max).unwrap();
+//! let edt_img = edt_image
+//!     .iter()
+//!     .map(|p| (*p / max_value * 255.) as u8)
+//!     .collect();
+//!
+//! let edt_img: ImageBuffer<Luma<u8>, Vec<u8>> =
+//!     ImageBuffer::from_vec(32, 32, edt_img).unwrap();
+//!
+//! // Write the contents of this image to the Writer in PNG format.
+//! edt_img.save("edt.png").unwrap();
+//! ```
+//!
+//! See [examples](https://github.com/msakuta/rust-edt/tree/master/examples) folder for more.
+//!
+//! ## Literature
+//!
+//! doi=10.1.1.66.2644
+//!
+//! <https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.66.2644&rep=rep1&type=pdf>
+//!
+//! Section 7.7
+
 /// Produce an EDT from binary image
 pub fn edt(map: &[bool], shape: (usize, usize)) -> Vec<f64> {
     let horz_edt = horizontal_edt(map, shape);
