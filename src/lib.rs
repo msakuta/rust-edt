9fa59d52e190e2648dab449beace8e2d81eff210
msakuta <masahiro.sakuta@gmail.com>
2022-02-21 02:58:50 +0900

    Add a bit of documentation comment

--- src/lib.rs
+++ src/lib.rs
@@ -129,6 +129,8 @@ pub fn edt<T: BoolLike>(map: &[T], shape: (usize, usize), invert: bool) -> Vec<f
 }
 
 /// Squared EDT of a given image.
+/// 
+/// The interface is equivalent to [`edt`], but it returns squared EDT.
 ///
 /// It is more efficient if you only need squared edt, because you wouldn't need to compute square root.
 pub fn edt_sq<T: BoolLike>(map: &[T], shape: (usize, usize), invert: bool) -> Vec<f64> {
