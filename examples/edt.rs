3ad12a5fe5cf4649b6dedd9bc421f0c9165d0949
msakuta <masahiro.sakuta@gmail.com>
2022-02-22 00:36:34 +0900

    Add clap crate to the dependencies and make example script user friendly

--- examples/edt.rs
+++ examples/edt.rs
@@ -1,22 +1,30 @@
 // mod save_img;
 
+use clap::Parser;
 use edt::{edt, edt_fmm, edt_fmm_cb, FMMCallbackData};
 use image::{GenericImageView, ImageBuffer, Luma, Rgb};
-use std::{env, time::Instant};
+use std::time::Instant;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    #[clap(default_value = "Rust_Logo.png", help = "File name to apply EDT.")]
+    file_name: String,
+    #[clap(short, long, help = "Use Fast Marching method")]
+    fast_marching: bool,
+    #[clap(
+        short,
+        long,
+        help = "Steps in FMM to produce images in progress. Each image is prefixed by step number.
+Warning! don't put too small number, or it will produce lots of images!"
+    )]
+    progress_steps: Option<usize>,
+}
 
 fn main() -> std::io::Result<()> {
-    let file_name = env::args()
-        .skip(1)
-        .next()
-        .unwrap_or_else(|| "Rust_logo.png".to_string());
+    let args = Args::parse();
 
-    let use_fmm = env::args()
-        .skip(2)
-        .next()
-        .map(|s| s == "-e")
-        .unwrap_or(false);
-
-    let img = image::open(file_name).unwrap();
+    let img = image::open(args.file_name).unwrap();
     let dims = img.dimensions();
     println!("dimensions {:?}, color: {:?}", dims, img.color());
 
@@ -31,42 +39,46 @@ fn main() -> std::io::Result<()> {
 
     let mut i = 0;
 
-    let edt_f64 = if use_fmm {
-        edt_fmm_cb(
-            &slice,
-            (dims.0 as usize, dims.1 as usize),
-            true,
-            |FMMCallbackData {
-                 map: edt_f64,
-                 next_pixels,
-                 ..
-             }| {
-                if i % 500 == 0 {
-                    let max_value = edt_f64.iter().map(|p| *p).reduce(f64::max).unwrap().max(1.);
-                    let edt_u8: Vec<_> = edt_f64
-                        .iter()
-                        .map(|p| (*p / max_value * 255.) as u8)
-                        .collect();
+    let edt_f64 = if args.fast_marching {
+        if let Some(progress_steps) = args.progress_steps {
+            edt_fmm_cb(
+                &slice,
+                (dims.0 as usize, dims.1 as usize),
+                true,
+                |FMMCallbackData {
+                     map: edt_f64,
+                     next_pixels,
+                     ..
+                 }| {
+                    if i % progress_steps == 0 {
+                        let max_value =
+                            edt_f64.iter().map(|p| *p).reduce(f64::max).unwrap().max(1.);
+                        let edt_u8: Vec<_> = edt_f64
+                            .iter()
+                            .map(|p| (*p / max_value * 255.) as u8)
+                            .collect();
 
-                    let mut edt_img: ImageBuffer<Rgb<u8>, Vec<_>> =
-                        ImageBuffer::new(dims.0, dims.1);
-                    for (x, y, pixel) in edt_img.enumerate_pixels_mut() {
-                        let luma = edt_u8[(x + y * dims.0) as usize];
-                        *pixel = Rgb([luma, luma, luma]);
+                        let mut edt_img: ImageBuffer<Rgb<u8>, Vec<_>> =
+                            ImageBuffer::new(dims.0, dims.1);
+                        for (x, y, pixel) in edt_img.enumerate_pixels_mut() {
+                            let luma = edt_u8[(x + y * dims.0) as usize];
+                            *pixel = Rgb([luma, luma, luma]);
+                        }
+
+                        for pixel in next_pixels {
+                            edt_img[(pixel.col as u32, pixel.row as u32)] = Rgb([255, 0, 0]);
+                        }
+
+                        edt_img.save(&format!("edt{}.png", i)).unwrap();
                     }
+                    i += 1;
 
-                    for pixel in next_pixels {
-                        edt_img[(pixel.col as u32, pixel.row as u32)] = Rgb([255, 0, 0]);
-                    }
-
-                    // Write the contents of this image to the Writer in PNG format.
-                    edt_img.save(&format!("edt{}.png", i)).unwrap();
-                }
-                i += 1;
-
-                true
-            },
-        )
+                    true
+                },
+            )
+        } else {
+            edt_fmm(&slice, (dims.0 as usize, dims.1 as usize), true)
+        }
     } else {
         edt(&slice, (dims.0 as usize, dims.1 as usize), true)
     };
@@ -82,7 +94,6 @@ fn main() -> std::io::Result<()> {
     let edt_img: ImageBuffer<Luma<u8>, Vec<u8>> =
         ImageBuffer::from_vec(dims.0, dims.1, edt_img).unwrap();
 
-    // Write the contents of this image to the Writer in PNG format.
     edt_img.save("edt.png").unwrap();
 
     Ok(())
