212874869f9ca2e990e5075ec21783f1e2ef8299
msakuta <masahiro.sakuta@gmail.com>
2022-03-08 02:32:50 +0900

    Add relpos option to circle and cross examples

--- examples/cross.rs
+++ examples/cross.rs
@@ -1,23 +1,35 @@
-use edt::{edt, edt_fmm};
-use image::{ImageBuffer, Luma};
-use std::{env, time::Instant};
+use clap::Parser;
+use edt::{edt, edt_fmm, edt_relpos};
+use image::{ImageBuffer, Luma, Rgb};
+use std::time::Instant;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    #[clap(
+        short,
+        long,
+        default_value = "512",
+        help = "Size of the synthesized edt image"
+    )]
+    size: usize,
+    #[clap(short, long, help = "Use Fast Marching method")]
+    fast_marching: bool,
+    #[clap(short, long, help = "Make difference between exact and Fast Marching")]
+    diff: bool,
+    #[clap(
+        short,
+        long,
+        help = "Use relative vector to the nearest background. The output image will be rgb image wher red is edt intensity, blue is horizontal relative position and green is vertical relative position"
+    )]
+    use_relpos: bool,
+}
 
 fn main() {
-    const SIZE: usize = 512;
-
-    let size = env::args()
-        .skip(1)
-        .next()
-        .and_then(|s| s.parse().ok())
-        .unwrap_or(SIZE);
+    let args = Args::parse();
+    let size = args.size;
     let half_size = size / 2;
     let quater_size = (size / 4) as isize;
-
-    let use_fmm = env::args()
-        .skip(2)
-        .next()
-        .map(|s| s == "-e")
-        .unwrap_or(false);
 
     let mut map = vec![false; size * size];
 
@@ -29,9 +41,20 @@ fn main() {
         }
     }
 
+    (if args.use_relpos {
+        main_edt_relpos
+    } else {
+        main_edt
+    })(args, &map)
+}
+
+fn main_edt(args: Args, map: &[bool]) {
+    let use_fmm = args.fast_marching;
+    let size = args.size;
+
     let start = Instant::now();
 
-    let edt_f64 = if use_fmm { edt_fmm } else { edt }(&map, (size, size), false);
+    let edt_f64 = if use_fmm { edt_fmm } else { edt }(map, (size, size), false);
 
     let duration = start.elapsed().as_micros();
     println!("time: {:?}ms", duration as f64 / 1e3);
@@ -44,6 +67,38 @@ fn main() {
     let edt_img: ImageBuffer<Luma<u8>, Vec<u8>> =
         ImageBuffer::from_vec(size as u32, size as u32, edt_img).unwrap();
 
-    // Write the contents of this image to the Writer in PNG format.
+    edt_img.save("edt.png").unwrap();
+}
+
+fn main_edt_relpos(args: Args, map: &[bool]) {
+    let size = args.size;
+
+    let start = Instant::now();
+
+    let edt_f64 = edt_relpos(&map, (size, size), false);
+
+    let duration = start.elapsed().as_micros();
+    println!("time: {:?}ms", duration as f64 / 1e3);
+    let max_value = edt_f64.iter().map(|p| p.val).reduce(f64::max).unwrap();
+    let max_relpos = edt_f64
+        .iter()
+        .map(|p| p.relpos[0].abs().max(p.relpos[1].abs()))
+        .max()
+        .unwrap();
+    let edt_img = edt_f64
+        .iter()
+        .map(|p| {
+            [
+                (p.val / max_value * 255.) as u8,
+                (p.relpos[0] * 127 / max_relpos) as u8 + 127,
+                (p.relpos[1] * 127 / max_relpos) as u8 + 127,
+            ]
+        })
+        .flatten()
+        .collect();
+
+    let edt_img: ImageBuffer<Rgb<u8>, Vec<u8>> =
+        ImageBuffer::from_vec(size as u32, size as u32, edt_img).unwrap();
+
     edt_img.save("edt.png").unwrap();
 }
